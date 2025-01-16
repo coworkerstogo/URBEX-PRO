8a749cab8da0783515f467d402367cb1038e64aa
Queen-Ora <>
2025-01-16 11:35:23 +0000

    Couleurs globales definies

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Couleurs principales
+  static const Color primary = Color(0xFF9932F3); // Violet
+  static const Color secondary = Color(0xFF5FC88F); // Vert clair
+  static const Color darkBlue = Color(0xFF222A47); // Bleu foncé
+  static const Color redAccent = Color(0xFFB53B43); // Rouge accentué
+  static const Color brightRed = Color(0xFFF33235); // Rouge vif
+
+  //Couleurs du formulaire
+  static const Color lightPeach = Color(0xFFFFEBE4); // Pêche clair
+  static const Color orangeAccent = Color(0xFFF7931A); // Orange accentué
+  static const Color lightGreen = Color(0xFFDEF5E9); // Vert clair
+  static const Color greenAccent = Color(0xFF5FC88F); // Vert accentué
+  static const Color lightBlue = Color(0xFFEBECFF); // Bleu clair
+  static const Color lightPurple = Color(0xFF9F9DF3); // Violet clair
+
+
+  // Couleurs de base
+  static const Color background = Color(0xFFF5F5F5); // Gris clair
+  static const Color surface = Color(0xFFFFFFFF); // Blanc
+  static const Color error = Color(0xFFB00020); // Rouge
+
+  // Couleurs de texte
+  static const Color textPrimary = Color(0xFF000000); // Noir
+  static const Color textSecondary = Color(0xFF757575); // Gris foncé
+}
